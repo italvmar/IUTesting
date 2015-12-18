4857e62e9e95661d56e033810944cbaf0cd37f61
italvmar <it.alvmar@gmail.com>
2015-12-18 11:49:33 +0100

    Provisional para dibujar varios tableros con imagenes desde archivo. NO CARGA las imagenes

--- DibujaTableros/engine.js
+++ DibujaTableros/engine.js
@@ -1,10 +1,12 @@
-
+console.log("CARGA GAME");
 var Game = new function() { 
 
 //Declaramos nuestro array de tableros
 //Meteremos aqui los tableros por el orden que deben ser pintados                                                                 
   var boards = [];
-
+  this.countboards= function(){
+    return boards.length;
+  }
 
   //Recogemos el canvas definido en el html e inicializamos la spritesheet
   //Aqui habria que cargar nuestra spriete sheet cuando la tengamos metida
@@ -18,12 +20,15 @@ var Game = new function() {
     this.width = this.canvas.width;
     this.height= this.canvas.height;
 
+    SpriteSheet.load(sprite_data,callback);
+
     //Simple formalidad
     this.ctx = this.canvas.getContext && this.canvas.getContext('2d');
     if(!this.ctx) { return alert("Please upgrade your browser to play"); }
 
     //Aqui se pondria el reconocimento de teclas
     //this.setupInput();
+    console.log("LLAMA A LOOP");
     this.loop(); 
 
     //SpriteSheet.load(sprite_data,callback);
@@ -35,9 +40,10 @@ var Game = new function() {
   //Cada  dt tiempo cambiamos 
   this.loop = function() { 
     var dt = 30 / 1000;
-
+    console.log(boards.length);
     for(var i=0,len = boards.length;i<len;i++) {
       if(boards[i]) { 
+        console.log("ITERA BUCLE");
          boards[i].draw(Game.ctx);
       }
     }
@@ -46,6 +52,32 @@ var Game = new function() {
   };
 
   // Change an active game board
-  this.setBoard = function(num,board) { boards[num] = board; };
+  this.setBoard = function(num,board) {
+    boards[num] = board;
+    console.log("sets boards");
+    console.log(boards.length) ;
+  };
+  
 };
 
+var SpriteSheet = new function() {
+  this.map = { }; 
+
+  this.load = function(spriteData,callback) { 
+    this.map = spriteData;
+    this.image = new Image();
+    this.image.onload = callback;
+    this.image.src = 'images/sprites.png';
+  };
+
+  this.draw = function(ctx,sprite,x,y,frame) {
+    var s = this.map[sprite];
+    if(!frame) frame = 0;
+    ctx.drawImage(this.image,
+                     s.sx + frame * s.w, 
+                     s.sy, 
+                     s.w, s.h, 
+                     x,   y, 
+                     s.w, s.h);
+  };
+}
--- DibujaTableros/game.js
+++ DibujaTableros/game.js
@@ -1,4 +1,6 @@
-
+var sprites = {
+ ship: { sx: 0, sy: 0, w: 37, h: 42, frames: 1 }
+};
 //Estamos Creando y metiendo tableros 
 var startGame = function() {
     //Pongamos que el primer tablero es el tapete
@@ -7,26 +9,33 @@ var startGame = function() {
 
 
 
-  Game.setBoard(0,Drawboard(url,"images/board.jpg"),300,300);
-  Game.setBoard(1,Drawboard(url,"images/01.jpg"),200,200);
-  Game.setBoard(2,Drawboard(url,"images/13.jpg"),100,100);
+  Game.setBoard(0,new Drawboard("images/board.jpg"),300,300);
+  Game.setBoard(1,new Drawboard("images/01.jpg"),200,200);
+  Game.setBoard(2,new Drawboard("images/13.jpg"),100,100);
+  
 }
 
-var Drawboard = function(tipe,url,width,height) {
+var Drawboard = function(tipe,url,wid,heig) {
 
   //var board = document.createElement("canvas");
   //var ctx = board.getContext("2d");
   board = new Image();
   board.src = url;
+  width= wid;
+  height=heig;
 
   // llamamos a esto con el ctx del canvas
   //para pintar los tableros
   this.draw = function(ctx) {
-    // Dibuja la imagen que sea   
-     ctx.drawImage(board,0, 0,width,height);
+    // Dibuja la imagen que sea  
+    console.log("ESTA LLAMANDO A DRAW") ;
+     ctx.drawImage(this.board,0, 0,this.width,this.height);
     }
 
 }
 
   
+window.addEventListener("load", function() {
+  Game.initialize("game",sprites,startGame);
 
+});
